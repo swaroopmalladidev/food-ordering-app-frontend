e6fb56fe294a48671ce890a0b49543d37149f9e1
mahesh2225 <g.mahendra22@gmail.com>
2020-03-05 00:06:03 +0530

    Adding api urls

---
+++ src/common/Apiurls.js
@@ -0,0 +1,7 @@
+export const constants = {
+    loginUrl: "http://localhost:8080/api/customer/login",
+    restaurantUrl: "http://localhost:8080/api/restaurant",   
+    registerUrl: "http://localhost:8080/api/customer/signup",
+    findRestaurant: "http://localhost:8080/api/restaurant/name",     
+    allrestaurantsUrl: "http://localhost:8080/api/restaurant"       
+};
